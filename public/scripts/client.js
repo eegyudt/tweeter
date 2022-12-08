e2371b9aa32a5270d3c55afb0218880d51e9efdd
Eszter Egyud <eszter.egyud@telus.com>
2022-12-07 19:42:07 -0700

    Implement listener for submit event and prevent its default behaviour, AJAX POST request to send data to server

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -8,14 +8,14 @@
 /* global document */
 
 
-$(() => {
+$(document).ready(function() {
 
   // console.log('hello');
 
-  const $tweetSection = $('.tweet-container');
+  // const $tweetSection = $('.tweet-container');
 
 
-  const createTweet = (tweet) => {
+  const createTweet = function(tweet) {
     console.log(tweet);
     const $tweet = $(`
   
@@ -31,7 +31,7 @@ $(() => {
             <content class="tweet-text" name="tweetText">${tweet.content.text}</content>
           </p>
           <footer>
-            <div>${timeago.format(tweet.created_at)}</div>
+          <div>${timeago.format(tweet.created_at)}</div>
             <div class="image-class">
               <i class="fa-solid fa-flag"></i>
               <i class="fas fa-retweet"></i>
@@ -45,23 +45,50 @@ $(() => {
     return $tweet;
   };
 
-  const renderTweets = (tweets) => {
+  const renderTweets = function(tweets) {
+    $('.tweet-container').empty();
     for (const tweet of tweets) {
       const $tweet = createTweet(tweet);
-      const $tweetSection = $('.tweet-container');
-      $tweetSection.append($tweet);
+      // const $tweetSection = $('.tweet-container');
+      $('.tweet-container').prepend($tweet);
     }
   };
 
 
-  const fetchTweets = () => {
-    $.get('/tweets', (tweets) => {
-      // console.log(tweets);
-      renderTweets(tweets);
-    });
+  $('#form').submit(function(event) {
+    event.preventDefault();
+    // get the data from the form (urlencoded data)
+    const data = $(this).serialize();
+    // make a post request to the server
+    $("#form")[0].reset();
+    $.ajax({
+      method: 'post',
+      url: '/tweets',
+      data
+    }).then(fetchTweets);
+
+    // $.post('/tweets', data, (response) => {
+    //   console.log(response);
+    // fetchTweets(); // GET
+  });
+
+  const fetchTweets = function() {
+    $.ajax({
+      method: 'get',
+      url: '/tweets',
+    }).then(renderTweets);
+    // $.get('/tweets', function(tweets) {
+    //   // console.log(tweets);
+    //   renderTweets(tweets);
+    // });
   };
+
 
   fetchTweets();
 
- 
 });
+
+
+
+
+
