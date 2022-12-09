66f74435580ff7fb16cf6ba3bcb0569a2f611371
Eszter Egyud <eszter.egyud@telus.com>
2022-12-09 15:24:39 -0700

    Correct container overflow, center user name under avatar

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,10 +1,9 @@
 /* global $ */
 /* global document */
 
+// Character counter
 $(document).ready(function() {
-  // let counter = $('#counter');
-  // console.log('composer-char-counter is loaded into indexed.html');  //not showing on console!!!
-  
+
   $("#tweet-text").on("input", function() {
     const maxLength = 140;
     const tweetLength = $(this).val().length;
@@ -15,7 +14,6 @@ $(document).ready(function() {
     let currentCount = $('.currentCounter');
     currentCount.html(maxLength);
 
-    // how do I do this in a css file?
     if (textRemaining < 0) {
       counter.css('color', '#FF0000');
       currentCount.css('color', '#FF0000');
@@ -23,8 +21,5 @@ $(document).ready(function() {
       counter.css('color', '#000000');
       currentCount.css('color', '#000000');
     }
-
   });
-
-
 });
